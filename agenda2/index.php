e7284b12b0f8751be316b0ce28b2d9da900c19f8
Jecorna <jecornba@gmail.com>
2017-07-18 23:35:40 -0500

    Se agregan componentes

---
+++ agenda2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/estilos.css" media="screen">
+  </head>
+  <body>
+    <div class="contendor">
+      <h1>Agenda de Contactos</h1>
+      <div class="contenido">
+        <h2>Nuevo Contacto</h2>
+        <form class="" action="crear.php" method="post">
+          <div class="campo">
+            <label for="nombre">Nombre
+              <input type="text" name="Nombre" placeholder="Nombre" id="nombre">
+            </label>
+          </div>
+          <div class="campo">
+            <label for="numero">Numero
+              <input type="text" name="Nombre" placeholder="Nombre" id="nombre">
+            </label>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
